fed5c2e18f1c209be5de3b26a390e56202caaad2
Danii1314 <d.silva.andres73@hotmail.com>
2025-05-11 01:26:02 -0500

    inicio funcional

--- app/controllers/validar_sesion.php
+++ app/controllers/validar_sesion.php
@@ -20,15 +20,30 @@ if (isset($_POST['correo']) && isset($_POST['contrasena'])) {
         // Obtener los datos del usuario
         $usuario = $result->fetch_assoc();
 
-  
-
-        // Verificar si la contraseña es correcta (usamos password_verify si las contraseñas están encriptadas)
-        if ($contrasena== $usuario['contrasena']) {
-            // Contraseña correcta, redirigir a otra página (por ejemplo, el panel del usuario)
+        // Verificar si la contraseña es correcta
+        if ($contrasena == $usuario['contrasena']) {
+            // Contraseña correcta, redirigir a otro apartado dependiendo del cargo
             session_start();  // Iniciar la sesión para guardar datos de sesión
             $_SESSION['usuario_id'] = $usuario['id_usuario'];  // Almacenar el ID de usuario en la sesión
             $_SESSION['usuario_email'] = $usuario['email'];  // Almacenar el correo en la sesión
-            header('Location: /Visualestudio/2entregablephp/assets/php/ApartadoConductor/Inicio.php');  // Redirigir al panel del usuario
+            $_SESSION['usuario_cargo'] = $usuario['id_cargo'];  // Almacenar el cargo del usuario
+
+            // Redirigir según el cargo del usuario
+            switch ($usuario['id_cargo']) {
+                case 1: // Gerente
+                    header('Location: /Visualestudio/2entregablephp/assets/php/ApartadoGerente/Inicio.php');
+                    break;
+                case 2: // Asistente
+                    header('Location: /Visualestudio/2entregablephp/assets/php/ApartadoAsistente/auxiliaresAsistente.php');
+                    break;
+                case 3: // Conductor
+                    header('Location: /Visualestudio/2entregablephp/assets/php/ApartadoConductor/Inicio.php');
+                    break;
+                
+                default:
+                    echo "Cargo no reconocido.";
+                    break;
+            }
             exit();  // Asegúrate de agregar exit() después de header() para detener el script
         } else {
             // Contraseña incorrecta
